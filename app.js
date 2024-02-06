e1a6cb2a39f950e2341f6b4ac90382af3cbebe61
emontes22 <ervey025@gmail.com>
2024-02-05 20:30:15 -0700

    Add grid for the game area

---
+++ app.js
@@ -0,0 +1,9 @@
+const grid = document.querySelector('.grid');
+const results = document.querySelector('.results');
+const width = 15;
+
+
+for (let i = 0; i < width * width; i++) {
+    donst = square = document.createElement('div');
+    grid.appendChild(square);
+}
